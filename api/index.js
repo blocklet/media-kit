2bccc71537c1e6bfdf325b40164bb1f891daea2a
wangshijun <wangshijun2010@gmail.com>
2023-07-28 11:48:28 +0800

    feat: disable webap auto convert

--- api/index.js
+++ api/index.js
@@ -9,7 +9,6 @@ const cookieParser = require('cookie-parser');
 const fallback = require('@blocklet/sdk/lib/middlewares/fallback');
 
 const { name, version } = require('../package.json');
-const { any2webp } = require('./libs/image');
 const logger = require('./libs/logger');
 const env = require('./libs/env');
 
@@ -23,59 +22,6 @@ app.set('trust proxy', true);
 app.use(cookieParser());
 app.use(express.json({ limit: env.maxUploadSize }));
 app.use(express.urlencoded({ extended: true, limit: env.maxUploadSize }));
-
-// Tasks of converting webp
-const webpTasks = {};
-
-// Convert images to webp on the fly
-// eslint-disable-next-line consistent-return
-app.use('/uploads/:filename', (req, res, next) => {
-  if (req.query.f !== 'webp') {
-    return next();
-  }
-
-  if (req.accepts('image/webp') === false) {
-    return next();
-  }
-
-  // already webp?
-  if (req.params.filename.endsWith('.webp')) {
-    return next();
-  }
-
-  const srcPath = path.join(env.uploadDir, req.params.filename);
-  // source not exist
-  if (fs.existsSync(srcPath) === false) {
-    return next();
-  }
-  // cannot convert to webp
-  if (['.png', '.jpg', '.jpeg'].some((ext) => srcPath.endsWith(ext)) === false) {
-    return next();
-  }
-
-  // already converted?
-  const destPath = path.join(env.uploadDir, `${req.params.filename}.webp`);
-  if (fs.existsSync(destPath)) {
-    return res.sendFile(destPath, { maxAge: '356d', immutable: true });
-  }
-
-  // do the convert
-  webpTasks[destPath] ??= any2webp(srcPath, destPath).finally(() => {
-    setTimeout(() => {
-      delete webpTasks[destPath];
-    }, 1000);
-  });
-
-  webpTasks[destPath]
-    .then(() => {
-      logger.info(`Converted ${srcPath} to webp`);
-      res.sendFile(destPath, { maxAge: '356d', immutable: true });
-    })
-    .catch((err) => {
-      console.error(`Failed to convert ${srcPath} to webp`, err);
-      next();
-    });
-});
 
 app.use('/uploads', express.static(env.uploadDir, { maxAge: '356d', immutable: true, index: false }));
 
