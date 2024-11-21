d35d4fbb59bc1e711dccd9d960bdb9bd36ce4ee5
LiangYongzhuo <firetable@foxmail.com>
2024-11-21 14:02:36 +0800

    chore: update deps (#325)
    * chore: update deps
    
    * chore: add svgr
    
    * chore: deduplicate deps

--- blocklets/image-bin/vite.config.mjs
+++ blocklets/image-bin/vite.config.mjs
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { createBlockletPlugin } from 'vite-plugin-blocklet';
 import { join } from 'path';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -15,7 +16,7 @@ export default defineConfig(({ mode }) => {
         allow: ['../../'], // monorepo and pnpm required
       },
     },
-    plugins: [react(), createBlockletPlugin()],
+    plugins: [react(), createBlockletPlugin(), svgr()],
     ...(mode === 'development' && {
       resolve: {
         alias: [
